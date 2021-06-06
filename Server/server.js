5a4427d11d35d0ce3806a49d25f6711d6e5a9805
unknown <marius.iurea@yahoo.com>
2021-06-06 17:02:09 +0300

    fixed same path with different methods conflicting and added gitignore

--- Server/server.js
+++ Server/server.js
@@ -10,10 +10,46 @@ var router = app.Router();
 router.handle('/test', 'get', (req, res) => {
     return res.end('test something');
 });
+router.handle('/test', 'POST', (req, res) => {
+    var postData = '';
+    req.on('data', (chunk) => {
+        postData += chunk;
+    });
+    req.on('data', () => {
+        var JSONdata = JSON.parse(postData);
+        console.log(JSONdata);
+        return res.end('test something' + JSON.stringify(JSONdata));
+    });
+});
+router.handle('/test', 'get', (req, res) => {
+    return res.end('test something');
+});
+router.handle('/test', 'POST', (req, res) => {
+    var postData = '';
+    req.on('data', (chunk) => {
+        postData += chunk;
+    });
+    req.on('data', () => {
+        var JSONdata = JSON.parse(postData);
+        console.log(JSONdata);
+        return res.end('test something' + JSON.stringify(JSONdata));
+    });
+});
 // format for url parameters
 // http://localhost:4200/test/23
 router.handle('/test/:testId', 'get', (req, res) => {
     return res.end(`test something ${req.params.testId}`);
+});
+router.handle('/test/:testId', 'post', (req, res) => {
+    var postData = '';
+    req.on('data', (chunk) => {
+        postData += chunk;
+    });
+    req.on('data', () => {
+        var JSONdata = JSON.parse(postData);
+        console.log(JSONdata);
+        return res.end(req.params.testId + 'test something' + JSON.stringify(JSONdata));
+    });
 });
 // format for querystring
 // http://localhost:4200/test?q=100&a=1000
